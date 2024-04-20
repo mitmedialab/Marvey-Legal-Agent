8d8a1c0dd6cf9de58a4c9f8c0210fb9160926476
Swapneel Bobba <swapneel.bobba@proton.me>
2024-04-20 16:03:51 -0700

    more ui setup + add button, dialog

---
+++ frontend/components/template-modal.tsx
@@ -0,0 +1,206 @@
+import React, { useCallback, useState, FC } from "react";
+import { Button } from "@/components/ui/button";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useDropzone } from "react-dropzone";
+import { FilePlusIcon } from "@radix-ui/react-icons";
+import axios from "axios";
+import { useUser } from "@clerk/nextjs";
+import { getTemplateFilePublicUrl } from "@/lib/utils";
+import { supabase } from "@/lib/supabase";
+import { toast } from "./ui/use-toast";
+
+const TemplateModalSchema = z.object({
+  templateName: z.string().min(2, {
+    message: "Template name must be at least 2 characters.",
+  }),
+  templateDescription: z.string().min(2, {
+    message: "Template name must be at least 2 characters.",
+  }),
+});
+
+interface Props {
+  disabled: boolean;
+}
+
+export const TemplateModal: FC<Props> = ({ disabled = false }) => {
+  const { user } = useUser();
+  const [loadingSubmit, setLoadingSubmit] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    // Do something with the files
+  }, []);
+  const { getRootProps, getInputProps, isDragActive, acceptedFiles } =
+    useDropzone({ onDrop });
+
+  const form = useForm<z.infer<typeof TemplateModalSchema>>({
+    resolver: zodResolver(TemplateModalSchema),
+    defaultValues: {
+      templateName: "",
+      templateDescription: "",
+    },
+  });
+
+  const files = acceptedFiles.map((file) => (
+    // @ts-ignore
+    <li key={file.path}>
+      {/*  @ts-ignore */}
+      {file.path}
+      {/* {file.path} - {file.size} bytes */}
+    </li>
+  ));
+
+  async function onSubmit(data: z.infer<typeof TemplateModalSchema>) {
+        });
+
+      if (error) {
+        console.error(error);
+        toast({
+          title: "Failed to upload template... ",
+          variant: "destructive",
+        });
+        return;
+      }
+
+      const templateUrl = await getTemplateFilePublicUrl(storageData.path!);
+
+      // store template in db
+      const response = await axios.post("/api/template", {
+        attorneyEmail: user?.primaryEmailAddress?.emailAddress || "",
+        templateName: data.templateName || "",
+        templateUrl: templateUrl?.publicUrl || "",
+        templateDescription: data.templateDescription || "",
+      });
+
+      setLoadingSubmit(false);
+      setOpen(false);
+      toast({
+        title: "Successfully uploaded template! ",
+      });
+    } catch (err: unknown) {
+      console.error(err);
+      toast({
+        title: "Something went wrong... ",
+        variant: "destructive",
+      });
+    }
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button className="mt-6" disabled={disabled}>
+          Upload Template
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-xl">
+        <DialogHeader>
+          <DialogTitle>Upload Template</DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="templateName"
+              render={({ field }) => (
+                <FormItem className="my-6">
+                  <FormLabel htmlFor="templateName" className="text-right">
+                    Template Name
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      id="templateName"
+                      placeholder="Ex: Jane Doe"
+                      className="col-span-3 mt-3"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="templateDescription"
+              render={({ field }) => (
+                <FormItem className="my-6">
+                  <FormLabel
+                    htmlFor="templateDescription"
+                    className="text-right"
+                  >
+                    Template Description
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      id="templateDescription"
+                      placeholder="Ex: Jane Doe"
+                      className="col-span-3 mt-3"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div
+              {...getRootProps()}
+              className="border border-gray-300 p-6 rounded-lg bg-gray-50 my-6"
+            >
+              {/* <PlusIcon className="mx-auto w-12 h-12 text-gray-400" /> */}
+              <FilePlusIcon className="mx-auto mb-3 w-12 h-12 text-gray-400/70" />
+
+              <input {...getInputProps()} />
+
+              <p className="text-center text-gray-500 text-sm">
+                {acceptedFiles.length
+                  ? files
+                  : isDragActive
+                  ? "Drop the files here ..."
+                  : ""}
+                {!acceptedFiles.length &&
+                  !isDragActive &&
+                  "Drag and drop files or click to upload"}
+              </p>
+            </div>
+            <DialogFooter className="flex sm:flex-row sm:justify-center">
+              <Button
+                type="submit"
+                disabled={
+                  !!form?.formState?.errors?.templateDescription ||
+                  !!form?.formState?.errors?.templateName ||
+                  loadingSubmit
+                }
+              >
+                Upload
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default TemplateModal;
